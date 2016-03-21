bc78c07022cfba230e93479c0bee3af376b3b2da
Scott <sco.suen.lab@gmail.com>
2016-03-20 21:56:39 -0400

    basic-features_1

--- learningredis/src/main/java/com/scott/learningredis/App.java
+++ learningredis/src/main/java/com/scott/learningredis/App.java
@@ -1,13 +1,62 @@
 package com.scott.learningredis;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		keyValue();
+		increment();
+		ttlNExpire();
+		set();
+	}
+
+	private static void keyValue() {
+		Jedis jedis = new Jedis("localhost");
+		jedis.set("key", "value");
+		System.out.println(jedis.get("key"));
+	}
+
+	private static void increment() {
+		Jedis jedis = new Jedis("localhost");
+		System.out.println(jedis.get("counter"));
+		jedis.incr("counter");
+		System.out.println(jedis.get("counter"));
+	}
+
+	private static void ttlNExpire() throws InterruptedException {
+		String cacheKey = "cachekey";
+		Jedis jedis = new Jedis("localhost");
+		jedis.set(cacheKey, "cached value");
+		// setting the TTL in seconds
+		jedis.expire(cacheKey, 5);
+		// Getting the remaining ttl
+		System.out.println("TTL:" + jedis.ttl(cacheKey));
+		Thread.sleep(1000);
+		System.out.println("TTL:" + jedis.ttl(cacheKey));
+		// Getting the cache value
+		System.out.println("Cached Value:" + jedis.get(cacheKey));
+
+		// Wait for the TTL finishs
+		Thread.sleep(5000);
+
+		// trying to get the expired key
+		System.out.println("Expired Key:" + jedis.get(cacheKey));
+	}
+
+	private static void set() {
+		String cacheKey = "languages";
+		Jedis jedis = new Jedis("localhost");
+		jedis.sadd(cacheKey, "Java", "C#", "Python");// SADD
+
+		// Getting all values in the set: SMEMBERS
+		System.out.println("Languages: " + jedis.smembers(cacheKey));
+		// Adding new values
+		jedis.sadd(cacheKey, "Java", "Ruby");
+		// Getting the values... it doesn't allow duplicates
+		System.out.println("Languages: " + jedis.smembers(cacheKey));
+	}
 }
